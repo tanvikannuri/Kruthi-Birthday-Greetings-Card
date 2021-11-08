68193979345e8e744a1135e330a1c804e159e929
tanvikannuri <72737567+tanvikannuri@users.noreply.github.com>
2021-11-08 10:44:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Gnana Kruthi" && b=="Spider Man")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
